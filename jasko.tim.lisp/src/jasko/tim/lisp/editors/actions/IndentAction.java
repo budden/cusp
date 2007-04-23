120a97c6c757ca95c4864519a155b80d971d75e4
cemerick <cemerick@snowtide.com>
2007-04-23 15:18:14 +0000

    Chas Emerick - Changed IndentAction to prevent spacing in multiline strings from being clobbered

--- jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/IndentAction.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/IndentAction.java
@@ -2,6 +2,7 @@ package jasko.tim.lisp.editors.actions;
 
 import jasko.tim.lisp.util.*;
 import jasko.tim.lisp.editors.LispEditor;
+import jasko.tim.lisp.editors.LispPartitionScanner;
 import jasko.tim.lisp.editors.assist.*;
 
 import org.eclipse.jface.text.*;
@@ -35,6 +36,14 @@ public class IndentAction extends LispAction {
 			for (int funcLine = firstLine; funcLine <= lastLine; ++funcLine) {
 				IRegion lineInfo = doc.getLineInformation(funcLine);
 				
+                // do not mess with indentation of multiline strings! 
+                // todo -- it might be cleaner architecturally to have this check happen in LispUtil,
+                //     but the code there is still somewhat opaque to me at the moment, and clobbering spacing in 
+                //     multiline strings can be *really* bad, so let expediency overtake purity for the nonce.
+                //     - Chas Emerick
+                if (doc.getContentType(lineInfo.getOffset()).equals(LispPartitionScanner.LISP_STRING) &&
+                        !doc.get(lineInfo.getOffset(), lineInfo.getLength()).trim().startsWith("\"")) continue;
+                
 				LispUtil.FunctionInfo fi = LispUtil.getCurrentFunctionInfo(doc, lineInfo.getOffset());
 				
 				String indent = LispIndenter.calculateIndent(fi, doc);
