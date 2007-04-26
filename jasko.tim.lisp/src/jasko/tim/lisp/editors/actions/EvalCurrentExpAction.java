d3cf931596cbb115928b6bff7a8c8d360cb7d2eb
cemerick <cemerick@snowtide.com>
2007-04-26 20:12:41 +0000

    Chas Emerick - Implemented "Evaluate Current Expression" action; current expression is either:     a set of expressions defined by the current selection; the prior expression if the cursor     is just to the right of a close paren; the next expression if the cursor is at an open paren;     or the most-local complete expression otherwise - Eliminated the empty popup that was appearing after evaluating the toplevel expression

---
+++ jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/EvalCurrentExpAction.java
@@ -0,0 +1,33 @@
+package jasko.tim.lisp.editors.actions;
+
+import jasko.tim.lisp.editors.LispEditor;
+import jasko.tim.lisp.swank.LispNode;
+import jasko.tim.lisp.swank.SwankRunnable;
+
+public class EvalCurrentExpAction extends LispAction {
+    public EvalCurrentExpAction () {}
+    
+    public EvalCurrentExpAction (LispEditor editor) {
+        super(editor);
+    }
+    
+    public void run() {
+        String exp = getCurrentExpression();
+        if (exp.length() == 0) return;
+        
+        getSwank().sendEval(exp, new SwankRunnable() {
+            public void run() {
+                LispNode res = this.result.getf(":return").getf(":ok").getf(":present");
+                /*  this used to display the result of the evaluation -- that already goes to the repl now,
+                 * and this value is always an empty string -- maybe a swank api change?
+                String display = "";
+                for (LispNode kid: res.params) {
+                    display += kid.get(0).value + "\n";
+                }
+                display = display.trim();
+                editor.showPopupInfo("=> " + display);
+                */
+            }
+        });
+    }
+}
