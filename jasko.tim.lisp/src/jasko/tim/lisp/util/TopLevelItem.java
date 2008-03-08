3bcbdd03b49c8f0f60fa9031d5685bf1a4b2bfdb
sergey.kolos@gmail.com <sergey.kolos@gmail.com@230afaf4-6840-11de-89a9-0b20f3e2dceb>
2008-03-08 05:53:54 +0000

    Sergey Kolos  - Fixed a bug in outline: when new function added to code, it is shown in outline, but tooltip did not work.

--- jasko.tim.lisp/src/jasko/tim/lisp/util/TopLevelItem.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/util/TopLevelItem.java
@@ -2,9 +2,9 @@ package jasko.tim.lisp.util;
 
 public class TopLevelItem implements Comparable<TopLevelItem> {
 	public String name;
-	public int offset;
-	public int offsetEnd;
-	public int nameOffset;
+	public int offset; //offset of "(" encompassing top level sexp
+	public int offsetEnd; //offset of ")" encompassing top level sexp
+	public int nameOffset; //offset of name of top level sexp
 	public String type;
 	public String pkg;
 	
