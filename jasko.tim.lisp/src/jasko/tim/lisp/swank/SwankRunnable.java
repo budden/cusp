f78d45295a11797ea7405c00de6bcc60d642c5e9
sergey.kolos@gmail.com <sergey.kolos@gmail.com@230afaf4-6840-11de-89a9-0b20f3e2dceb>
2008-08-14 20:37:21 +0000

    Scott Michel:  - Refactored action bindings in ReplView  - Some minor changes to cleanup code

--- jasko.tim.lisp/src/jasko/tim/lisp/swank/SwankRunnable.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/swank/SwankRunnable.java
@@ -3,7 +3,7 @@ package jasko.tim.lisp.swank;
 /**
  * Commands sent to Swank are associated with an instance of some descendant of this class.
  * The result of that command will be stuck in to the result member of the class,
- *  and then SwankInterface will <i>attempt</i> to clone the SwnakRunnable via the
+ *  and then SwankInterface will <i>attempt</i> to clone the SwankRunnable via the
  *  clone method. It should be noted that this tends to fail with anonymous classes,
  *  so don't use those where certain threading issues are a concern.
  * The SwankRunnable will then be run in the main GUI thread, thus avoiding
