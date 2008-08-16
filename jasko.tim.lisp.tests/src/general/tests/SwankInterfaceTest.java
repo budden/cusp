86f2e1b0cbac2605fe22f3b5896a1d03e8d800f8
sergey.kolos@gmail.com <sergey.kolos@gmail.com@230afaf4-6840-11de-89a9-0b20f3e2dceb>
2008-08-16 17:15:22 +0000

    Sergey Kolos:  - Added test for swank's haveDefinitions() function.

--- jasko.tim.lisp.tests/src/general/tests/SwankInterfaceTest.java
+++ jasko.tim.lisp.tests/src/general/tests/SwankInterfaceTest.java
@@ -12,9 +12,15 @@ public class SwankInterfaceTest extends TestCase {
 		return LispPlugin.getDefault().getSwank();
 	}
 	
-	public void testSendEvalAndGrab() {
+	public void test_sendEvalAndGrab() {
 		String res = getSwank().sendEvalAndGrab("(+ 2 3)",1000);
 		assertTrue(res.equals("5"));
 	}
 
+	public void test_haveDefinitions() {
+		 // SK: on my system (WinXP, sbcl), I don't have SBCL sources, so
+		// res = false. It might be true with SBCL sources.
+		boolean res = getSwank().haveDefinitions("*query-io*", ":wiz", 2000);
+		assertTrue(!res);
+	}
 }
