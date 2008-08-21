b78c403dd60151e4d8a2e5be9840c5486eaaa89c
sergey.kolos@gmail.com <sergey.kolos@gmail.com@230afaf4-6840-11de-89a9-0b20f3e2dceb>
2008-08-21 01:44:18 +0000

    Sergey Kolos:  - Implemented CustomAutoEdit. In the end I found it is not as useful as I expected - I now have another plan for similar but more useful feature, but its implementation will need to wait.  How to use: Go to Preferences->Lisp->Auto Edits. Add your autoedit as follows: let;let ((|)). First part (before ';') triggers autoedit, the second part (after ';') replaces trigger (after '|' is removed) and the caret is placed at '|'.  Why is it not very useful? If you type 'letter', after you hit your first t, autoedit will trigger. Hitting Ctrl+z will get you back to 'le'. So there is not easy way to type anything that has 'let' in the middle. I am going to keep this feature for a moment, so that people could play with it and give feedback - maybe there are some uses that I don't see.

--- jasko.tim.lisp/src/jasko/tim/lisp/editors/autoedits/AllAutoEdits.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/editors/autoedits/AllAutoEdits.java
@@ -6,6 +6,6 @@ public class AllAutoEdits {
 	// this function is called when an editor is created
 	public static IAutoEditStrategy[] get(){
 		return 	new IAutoEditStrategy[] {new LispIndentOnTab(),
-				new LispIndentOnEnter(), new PairAutoEdit()};//, new CustomAutoEdit()};
+				new LispIndentOnEnter(), new PairAutoEdit(), new CustomAutoEdit()};
 	}
 }
