83edaa1e9d7544700195d384180730421cb0bfb8
sergey.kolos@gmail.com <sergey.kolos@gmail.com@230afaf4-6840-11de-89a9-0b20f3e2dceb>
2008-08-30 23:31:50 +0000

    Sergey Kolos: 	- Added Toggle Breakpoint editor action (key combination: Alt+B). To use: Select a sexp at which you want to break (using Alt+Enter). Hit Alt+B. The selection will be surrounded by (progn (break) sexp), and the code will break into debugger at break. To remove break, select whole progn form and hit Alt+B.

--- jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/BreakpointAction.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/BreakpointAction.java
@@ -6,13 +6,15 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.*;
 import org.eclipse.ui.IEditorPart;
 
-public class CommentingAction extends LispAction {
 
+public class BreakpointAction extends LispAction {
     private ILispEditor editor;
+    private static String start = "(progn #|br|# (break) ";
+    private static String end = ")";
     
-    public CommentingAction () {}
+    public BreakpointAction () {}
     
-    public CommentingAction (ILispEditor editor) {
+    public BreakpointAction (ILispEditor editor) {
         this.editor = editor;
     }
     
@@ -29,11 +31,11 @@ public class CommentingAction extends LispAction {
         if( ts.getLength() > 0 ){
         	String oldtxt = ts.getText();
         	String txt = "";
-        	if( oldtxt.length() > 4 
-        			&& oldtxt.startsWith("#|") && oldtxt.endsWith("|#") ){
-        		txt = oldtxt.substring(2, oldtxt.length()-2);
+        	if( oldtxt.length() > start.length() + end.length()
+        			&& oldtxt.startsWith(start) && oldtxt.endsWith(end) ){
+        		txt = oldtxt.substring(start.length(), oldtxt.length()-end.length());
         	} else {
-            	txt = "#|" + oldtxt + "|#";        		
+            	txt = start + oldtxt + end;        		
         	}
         	try{
         		doc.replace(offset, ts.getLength(), txt);
@@ -44,5 +46,5 @@ public class CommentingAction extends LispAction {
                 e.printStackTrace();        		
         	}
         }
-    }    
+    }
 }
