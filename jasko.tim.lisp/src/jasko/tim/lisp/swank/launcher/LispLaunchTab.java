971a5f7a71a2c806dfd75ec50e4c00959a73df46
sergey.kolos@gmail.com <sergey.kolos@gmail.com@230afaf4-6840-11de-89a9-0b20f3e2dceb>
2008-09-03 02:32:29 +0000

    Sergey Kolos: 	- Launch is now usable, but I could not register it with perspective to provide shortcut. Will do next time.

--- jasko.tim.lisp/src/jasko/tim/lisp/swank/launcher/LispLaunchTab.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/swank/launcher/LispLaunchTab.java
@@ -1,17 +1,14 @@
 package jasko.tim.lisp.swank.launcher;
 
 import jasko.tim.lisp.*;
+import jasko.tim.lisp.swank.*;
 
+import java.io.File;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -24,14 +21,16 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.dialogs.ResourceListSelectionDialog;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.core.runtime.CoreException;
 
 public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 
 	private Text fProgramText;
 	private Button fProgramButton;
-	
+	private Combo fLispFlavor;
+	private static String flavors = "SBCL;CLISP;Allegro";
+	private String exename;
 	
 	public void createControl(Composite parent) {
 		Font font = parent.getFont();
@@ -39,16 +38,43 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 		Composite comp = new Composite(parent, SWT.NONE);
 		setControl(comp);
 		GridLayout topLayout = new GridLayout();
-		topLayout.verticalSpacing = 0;
+		topLayout.verticalSpacing = 5;
 		topLayout.numColumns = 3;
 		comp.setLayout(topLayout);
 		comp.setFont(font);
 		
 		createVerticalSpacer(comp, 3);
+
+		Label flavorLabel = new Label(comp,SWT.NONE);
+		flavorLabel.setText("Lisp &Flavor:");
+		GridData gd = new GridData(GridData.BEGINNING);
+		flavorLabel.setLayoutData(gd);
+		flavorLabel.setFont(font);
+		
+		fLispFlavor = new Combo(comp, SWT.DROP_DOWN | SWT.READ_ONLY);
+		fLispFlavor.setItems(flavors.split(";"));
+		fLispFlavor.select(0);
+		
+		Label tmpLabel = new Label(comp,SWT.NONE);
+		gd = new GridData(GridData.BEGINNING);
+		tmpLabel.setLayoutData(gd);
+		tmpLabel.setFont(font);
+		
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		fLispFlavor.setLayoutData(gd);
+		fLispFlavor.setFont(font);
+		fLispFlavor.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				updateLaunchConfigurationDialog();
+				if( fLispFlavor.getText().equalsIgnoreCase("SBCL") ){
+					updateProgramPathWithSBCL();
+				}
+			}
+		});
 		
 		Label programLabel = new Label(comp, SWT.NONE);
 		programLabel.setText("&Lisp Executable:");
-		GridData gd = new GridData(GridData.BEGINNING);
+		gd = new GridData(GridData.BEGINNING);
 		programLabel.setLayoutData(gd);
 		programLabel.setFont(font);
 		
@@ -61,6 +87,7 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 				updateLaunchConfigurationDialog();
 			}
 		});
+		updateProgramPathWithSBCL();
 		
 		fProgramButton = createPushButton(comp, "&Browse...", null); //$NON-NLS-1$
 		fProgramButton.addSelectionListener(new SelectionAdapter() {
@@ -70,18 +97,29 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 		});
 	}
 
+	private void updateProgramPathWithSBCL(){
+		File file = SBCLImplementation.findExecutable();
+		File folder = SBCLImplementation.findFolder();
+		if( file != null && folder != null ){
+			fProgramText.setText(folder.getPath()+ File.separator + file.getName());
+			exename = file.getName();
+		}		
+	}
+	
 	/**
 	 * Open a resource chooser to select a PDA program 
 	 */
 	protected void browseLispExeFiles() {
-		ResourceListSelectionDialog dialog = new ResourceListSelectionDialog(getShell(), 
-				ResourcesPlugin.getWorkspace().getRoot(), IResource.FILE);
-		dialog.setTitle("Lisp Executable");
-		dialog.setMessage("Select Lisp Executable");
-		if (dialog.open() == Window.OK) {
-			Object[] files = dialog.getResult();
-			IFile file = (IFile) files[0];
-			fProgramText.setText(file.getFullPath().toString());
+		FileDialog fd = new FileDialog(getShell());
+		fd.setFileName(fProgramText.getText());
+		if( exename != null && exename != ""){
+			fd.setFilterNames(new String[]{exename});
+		} else if( System.getProperty("os.name").toLowerCase().contains("windows") ){
+			fd.setFilterExtensions(new String[]{"*.exe"});
+		}
+		String fileName = fd.open();
+		if( fileName != null ){
+			fProgramText.setText(fileName);
 		}
 	}
 	
@@ -91,10 +129,19 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 
 	public void initializeFrom(ILaunchConfiguration configuration) {
 		try {
-			String program = null;
-			program = configuration.getAttribute(LispPlugin.ATTR_LISP_EXE, (String)null);
+			String program = configuration.getAttribute(LispPlugin.ATTR_LISP_EXE, (String)null);
 			if (program != null) {
 				fProgramText.setText(program);
+			}
+			String flavor = configuration.getAttribute(LispPlugin.ATTR_LISP_FLAVOR, (String)null);
+			if( flavor != null ){
+				String[] flavorList = flavors.split(";");
+				for( int i = 0; i < flavorList.length; ++i ){
+					if(flavor.matches(flavorList[i])){
+						fLispFlavor.select(i);
+						break;
+					}
+				}				
 			}
 		} catch (CoreException e) {
 			setErrorMessage(e.getMessage());
@@ -107,6 +154,7 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 			program = null;
 		}
 		configuration.setAttribute(LispPlugin.ATTR_LISP_EXE, program);
+		configuration.setAttribute(LispPlugin.ATTR_LISP_FLAVOR, fLispFlavor.getText());
 	}
 
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
@@ -120,14 +168,15 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 		setMessage(null);
 		String text = fProgramText.getText();
 		if (text.length() > 0) {
-			IPath path = new Path(text);
-			if (ResourcesPlugin.getWorkspace().getRoot().findMember(path) == null) {
+			File file = new File(text);
+			if( !file.exists() ){
 				setErrorMessage("Specified program does not exist");
 				return false;
-			}
+			};
 		} else {
-			setMessage("Specify a program");
+			setMessage("Specify lisp executable");
 		}
+
 		return true;
 	}
 
@@ -135,7 +184,8 @@ public class LispLaunchTab extends AbstractLaunchConfigurationTab {
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#getImage()
 	 */
 	public Image getImage() {
-		return LispPlugin.getDefault().getImageRegistry().get(LispImages.EXEC);
+		Image img = LispPlugin.getDefault().getImageRegistry().get(LispImages.EXEC); 
+		return img; 
 	}
 	
 }
