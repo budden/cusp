519dddf784d409c04928e9ab0852a255646ff8e4
sergey.kolos <sergey.kolos@gmail.com>
2008-09-26 00:36:13 +0000

    Sergey Kolos: 	- Reverting back second part of changeset [392]. New code that supposed to handle paren matching with string of octothorpes breaks on the following lisp code:
    (defun f (x) "xs = ((:N \"n\")),"x)
    
    Since I don't have any example of string of octothorpes I am reverting code back to what worked for me.

--- jasko.tim.lisp/src/jasko/tim/lisp/builder/LispBuilder.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/builder/LispBuilder.java
@@ -552,9 +552,9 @@ public class LispBuilder extends IncrementalProjectBuilder {
 							charOffset += line.length() - i;
 							i = line.length();
 							break;
-						} else if (c == '"' && !(i > 1 && (!inQuotes && (line.charAt(i-1) == '\\' || line.charAt(i-2) == '#')))) {
+						} else if (c == '"' && !(i > 1 && (line.charAt(i-1) == '\\' || line.charAt(i-2) == '#'))) {
 							inQuotes = !inQuotes;
-						} else if (c == '#' && !inQuotes && !(i > 1 && (line.charAt(i-1) == '\\' || line.charAt(i-2) == '#'))) {
+						} else if (c == '#' && !(i > 1 && (line.charAt(i-1) == '\\' || line.charAt(i-2) == '#'))) {
 							if (i+1 < line.length()) {
 								if (line.charAt(i+1) == '|') {
 									inComment = true;
